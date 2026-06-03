8f580eb54c7d0a8afe488fc12de6bae5d1f5c29c
Harshit-24 <harshitm24@outlook.com>
2026-06-04 00:41:21 +0530

    added backend URL

--- server/config/config.js
+++ server/config/config.js
@@ -4,10 +4,25 @@ const config = {
   port: process.env.PORT || 5000,
   env: process.env.NODE_ENV || "development",
   dataFilePath: path.join(__dirname, "../data/tasks.json"),
+
   corsOptions: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.CLIENT_URL],
     credentials: true,
   },
 };
 
 module.exports = config;
+
+// const path = require("path");
+
+// const config = {
+//   port: process.env.PORT || 5000,
+//   env: process.env.NODE_ENV || "development",
+//   dataFilePath: path.join(__dirname, "../data/tasks.json"),
+//   corsOptions: {
+//     origin: process.env.CLIENT_URL || "http://localhost:5173",
+//     credentials: true,
+//   },
+// };
+
+// module.exports = config;
